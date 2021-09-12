5ed26f94eac84c3e27b030620ad1b39ed7fd8719
yimkh <1256815659@qq.com>
2021-09-12 12:08:53 +0800

    add func
    add functions about isEmpty\isFull\Add\Insert

--- sqList/components/components.go
+++ sqList/components/components.go
@@ -2,23 +2,68 @@ package components
 
 import "fmt"
 
-var orders struct {
+var sqList struct {
 	maxSize int
-	form    []int
+	data    []int
 	length  int
 }
 
 func Init(maxSize int) {
-	orders.maxSize = maxSize
-	orders.form = make([]int, maxSize)
+	sqList.maxSize = maxSize
+	sqList.data = make([]int, maxSize)
 	for i := 0; i < maxSize; i++ {
-		orders.form[i] = 0
+		sqList.data[i] = 0
 	}
-	orders.length = 0
+	sqList.length = 0
 }
 
-func PrintOrder() {
-	for i := 0; i < orders.maxSize; i++ {
-		fmt.Printf("form[%v] = %v\n", i, orders.form[i])
+func PrintSqList() {
+	if IsEmpty() {
+		fmt.Println("empty!!!")
+	} else {
+		for i := 0; i < sqList.length; i++ {
+			fmt.Printf("data[%v] = %v\n", i, sqList.data[i])
+		}
+	}
+}
+
+func IsEmpty() bool {
+	if sqList.length == 0 {
+		return true
+	} else {
+		return false
+	}
+}
+
+func IsFull() bool {
+	if sqList.length >= sqList.maxSize {
+		return true
+	} else {
+		return false
+	}
+}
+
+func Add(data int) {
+	if IsFull() {
+		fmt.Println("full!!!")
+	} else {
+		sqList.data[sqList.length] = data
+		sqList.length++
+		PrintSqList()
+	}
+}
+
+func Insert(locat int, data int) {
+	if locat < 1 || locat > sqList.length+1 {
+		fmt.Println("locat is illegal")
+	} else if IsFull() {
+		fmt.Println("full!!!")
+	} else {
+		for i := sqList.length; i >= locat; i-- {
+			sqList.data[i] = sqList.data[i-1]
+		}
+		sqList.data[locat] = data
+		sqList.length++
+		PrintSqList()
 	}
 }
--- sqList/main.go
+++ sqList/main.go
@@ -4,5 +4,9 @@ import "data-structure/sqList/components"
 
 func main() {
 	components.Init(10)
-	components.PrintOrder()
+	components.PrintSqList()
+	for i := 0; i < 9; i++ {
+		components.Add(i)
+	}
+	components.Insert(5, 100)
 }
